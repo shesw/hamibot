95a4c812a523116a50cdcbb48785e9a12f14d338
shesw <335012480@qq.com>
2022-10-14 11:42:10 +0800

    [fun]测试项目添加运行广播

--- app/src/main/java/com/shesw/hamibot/hamibot_scripts_test/HamibotScriptsTestActivity.kt
+++ app/src/main/java/com/shesw/hamibot/hamibot_scripts_test/HamibotScriptsTestActivity.kt
@@ -1,9 +1,13 @@
 package com.shesw.hamibot.hamibot_scripts_test
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.hamibot.hamibot.R
 import com.hamibot.hamibot.autojs.AutoJs
 import com.hamibot.hamibot.external.ScriptIntents
@@ -13,12 +17,24 @@ class HamibotScriptsTestActivity : AppCompatActivity(), View.OnClickListener {
     private var btnRun: View? = null
     private var btnStop: View? = null
 
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            run()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hamibot_scripts_test)
         findView()
         initView()
         init()
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, IntentFilter("start_run_scripts"))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
     }
 
     private fun findView() {
